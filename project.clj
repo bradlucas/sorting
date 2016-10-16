dcfcb642de4b5571bb269eafd641b2f7a450d39c
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:20:27 -0400

    versino 1.8.0 of clojure

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject sorting "0.1.0-SNAPSHOT"
+(defproject sorting "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main sorting.core
 )
  
